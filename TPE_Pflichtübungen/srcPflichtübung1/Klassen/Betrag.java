4047bd4c1efc0da37d4c94e3e6296576fddcce94
Patricks-PC <Patricks-PC@DESKTOP-49E9DL7.local>
2016-03-16 22:39:29 +0100

    getVorzeichen implementiert

--- TPE_Pflichtübungen/srcPflichtübung1/Klassen/Betrag.java
+++ TPE_Pflichtübungen/srcPflichtübung1/Klassen/Betrag.java
@@ -10,38 +10,45 @@ public class Betrag {
 	private String waehrung;
 
 	/**
-	 * Der Konstruktor der Klasse Betrag 
+	 * Der Konstruktor der Klasse Betrag
+	 * 
 	 * @author ${Patrick Hentschel 1524045}
 	 * @param betrag
 	 *            gibt den Betrag des Kontostandes an
 	 * @param waehrung
-	 * 			  gibt die Waehrung des Betrags an 
+	 *            gibt die Waehrung des Betrags an
 	 */
 	Betrag(double betrag, String waehrung) {
 
 		this.betrag = (long) betrag;
 		this.waehrung = waehrung;
-		
+
 	}
 
 	/**
 	 * 
-	 * @author ${Patrick Hentschel 1524045} 
-	 * die Methode getVorzeichen gibt aus
-	 * ob der Kontostand positiv oder negativ ist mittels 1 oder -1
+	 * @author ${Patrick Hentschel 1524045} die Methode getVorzeichen gibt aus
+	 *         ob der Kontostand positiv oder negativ ist mittels 1 oder -1
 	 */
-	private int getVorzeichen() {
+	public int getVorzeichen() {
 		// TODO implementieren getVorzeichen
-		if(betrag > 1){	
+		if (betrag > 1) {
+			System.out.println("1");
 			return 1;
-		}else if(betrag < 1){
+		} else if (betrag == 0) {
+			System.out.println("0");
+			return 0;
+		} else if (betrag < 1) {
+			System.out.println("-1");
 			return -1;
-		}else if(betrag == 1){
+		} else if (betrag == 1) {
+			System.out.println("1");
 			return 1;
+
 		}
-		
+
 		return -0;
-		
+
 	}
 
 }
---
+++ TPE_Pflichtübungen/srcPflichtübung1/Klassen/Main.java
@@ -0,0 +1,11 @@
+package Klassen;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Betrag m = new Betrag(0.0, "Bla");
+		m.getVorzeichen();
+	}
+
+}
