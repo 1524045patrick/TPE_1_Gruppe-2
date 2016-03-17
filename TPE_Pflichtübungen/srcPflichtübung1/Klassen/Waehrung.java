7cdba704ea2752a34cae3f55676f824438195b51
student <student@192.168.101.21>
2016-03-17 13:50:33 +0100

    bitch is schuld

---
+++ TPE_Pflichtübungen/srcPflichtübung1/Klassen/Waehrung.java
@@ -0,0 +1,12 @@
+package Klassen;
+
+public class Waehrung {
+
+	public String name;
+	public String kuerzel;
+	public long kurs;
+	
+	public long umrechnen() {
+		
+	}
+}
---
+++ TPE_Pflichtübungen/srcPflichtübung1/Klassen/Waehrungen.java
@@ -0,0 +1,31 @@
+package Klassen;
+
+public final class Waehrungen extends Waehrung {
+
+
+	//name
+	String dollar = "Dollar";
+	String euro = "Euro";
+	String yen = "Yen";
+	String rubel = "Rubel";
+	String schweizerFranken = "Schweizer Franken";
+
+	//kuerzel
+	String $ = "$";
+	String € = "€";
+	String Y = "Y";
+	String RUB = "RUB";
+	String CHF = "CHF";
+
+	//kurs
+
+	double kursDollar = 1.0000;
+	double kursEuro = 1.2690;
+	double kursYen = 0.0091;
+	double kursRubel = 0.0255;
+	double kursSchweizerFranken = 1.0509;
+
+
+	}
+
+
