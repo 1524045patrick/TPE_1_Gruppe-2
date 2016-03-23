9edb22f0cf1df36b1901740b7a73663fda6c0435
Statos <Statos@localhost>
2016-03-23 19:38:53 +0100

    Konto Klasse

---
+++ TPE_Pflichtübungen/srcPflichtübung1/Klassen/Konto.java
@@ -0,0 +1,86 @@
+package Klassen;
+import java.util.ArrayList;
+/**
+ * 
+ * @author Dennis Szczerbinski
+ *
+ */
+public class Konto {
+
+	String inhaber;
+	String waehrung;
+	ArrayList<Integer> guthaben = new ArrayList<Integer>();
+	
+	/**
+	 * 
+	 * @param inhaber	Inhaber des Kontos
+	 * @param waehrung	Währung des Kontos
+	 */
+	Konto(String inhaber, String waehrung){
+		this.inhaber = inhaber;
+		this.waehrung = waehrung;
+	}
+	
+	/**
+	 * 
+	 * @return	Gibt den Inhaber des Kontos zurück
+	 */
+	public String getInhaber(){
+		return inhaber;
+	}
+	
+	/**
+	 * 
+	 * @return	Gibt die Währung des Kontos zurück
+	 */
+	public String getWaehrung(){
+		return waehrung;
+	}
+	
+	/**
+	 * 
+	 * @param a	Übergibt einen double-Wert der auf der ArrayList hinzugefügt
+	 * 			wird
+	 */
+	public void buche(double a){
+		guthaben.add((int) a);
+	}
+	
+	/**
+	 * 
+	 * @return	Gibt alle vorhanden Werte der ArrayList als String wieder
+	 */
+	public String saldo(){
+			return guthaben.toString();
+		
+	}
+	
+	/**
+	 * 
+	 * @return	Gibt die Summe der gesamten Buchungen zurück
+	 */
+	public double gesamt(){
+		double summe=0;
+		for(int i: guthaben){
+			summe += i;
+		}
+		return summe;
+	}
+		
+	
+	/**
+	 * 		Gibt das gesamte Konto aus mit Inhaber,Währung und Buchungen
+	 */
+	public String toString(){
+		System.out.println("Kontoinhaber :"+inhaber);
+		System.out.println("Währung :"+waehrung);
+		System.out.println("-------------");
+		saldo();
+		System.out.println("-------------");
+		System.out.println(gesamt());
+		return null;
+		
+		
+	}
+}
+
